e7f18c610c4f1cc4498189e8bfc84bd9bbdceac6
Md. Abdur Rahman <ardotrony@gmail.com>
2019-08-09 14:46:08 +0600

    Setting up basic index file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' )?>">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php wp_head();?>
+</head>
+<body>
+    
+    <?php wp_footer();?>
+</body>
+</html>
